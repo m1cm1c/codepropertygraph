39d9ccf1b73545b0d443f2e3213bb664d5d18868
Christoph Michelbach <michelbach94@gmail.com>
2021-01-19 12:06:56 +0100

    Fix operator name of index accesses.

--- fuzzyc2cpg/src/main/scala/io/shiftleft/fuzzyc2cpg/FuzzyC2Cpg.scala
+++ fuzzyc2cpg/src/main/scala/io/shiftleft/fuzzyc2cpg/FuzzyC2Cpg.scala
@@ -1087,13 +1087,13 @@ class FuzzyC2Cpg() {
         graph.node(BASE_ID + statementId).setProperty("ARGUMENT_INDEX", order)
         graph.node(BASE_ID + statementId).setProperty("CODE", code)
         graph.node(BASE_ID + statementId).setProperty("COLUMN_NUMBER", 0)
-        graph.node(BASE_ID + statementId).setProperty("METHOD_FULL_NAME", "<operator>.assignment")
+        graph.node(BASE_ID + statementId).setProperty("METHOD_FULL_NAME", "<operator>.indexAccess")
         graph.node(BASE_ID + statementId).setProperty("TYPE_FULL_NAME", "ANY")
         graph.node(BASE_ID + statementId).setProperty("LINE_NUMBER", 0)
         graph.node(BASE_ID + statementId).setProperty("DISPATCH_TYPE", "STATIC_DISPATCH")
         graph.node(BASE_ID + statementId).setProperty("SIGNATURE", "TODO assignment signature")
         graph.node(BASE_ID + statementId).setProperty("DYNAMIC_TYPE_HINT_FULL_NAME", List())
-        graph.node(BASE_ID + statementId).setProperty("NAME", "<operator>.assignment")
+        graph.node(BASE_ID + statementId).setProperty("NAME", "<operator>.indexAccess")
         order += 1
 
         graph.node(BASE_ID + statementId).addEdge("ARGUMENT", graph.node(BASE_ID + leftId))
